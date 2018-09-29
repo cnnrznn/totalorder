32e6c36186e148307a5d963bc677a415954e29a3
Connor Zanin <cnnrznn@gmail.com>
2018-09-29 13:53:06 -0400

    read hostfile; improve error cleanup

--- channel.c
+++ channel.c
@@ -1,6 +1,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -13,7 +14,29 @@ static struct addrinfo hints, *skaddr;
 int
 ch_init(char *hostfile, char *port)
 {
-        // TODO read hostfile
+        FILE *f;
+        size_t linelen = 32;
+        size_t strlen;
+        char *line;
+
+        // read hostfile
+        f = fopen(hostfile, "r");
+        if (NULL == f) {
+                perror("Unable to open hostfile");
+                goto err_open;
+        }
+
+        line = malloc(linelen);
+
+        while ((strlen = getline(&line, &linelen, f)) > 1) {
+                line[strlen-1] = '\0';
+                fprintf(stderr, "Read '%s(%d:%d)' from hostfile\n", line, linelen, strlen);
+        }
+
+        free(line);
+        fclose(f);
+
+        // allocate socket
 
         memset(&hints, 0, sizeof(hints));
         hints.ai_family = AF_INET;
@@ -21,24 +44,30 @@ ch_init(char *hostfile, char *port)
         hints.ai_flags = AI_PASSIVE;
 
         if (getaddrinfo(NULL, port, &hints, &skaddr)) {
-                perror("Unable to getaddrinfo()\n");
-                return -1;
+                perror("Unable to getaddrinfo()");
+                goto err_addr;
         }
         if ((sk = socket(skaddr->ai_family, skaddr->ai_socktype, skaddr->ai_protocol)) == -1) {
-                perror("Unable to create socket\n");
-                return -1;
+                perror("Unable to create socket");
+                goto err_addr;
         }
         if (bind(sk, skaddr->ai_addr, skaddr->ai_addrlen) == -1) {
-                perror("Unable to bind socket\n");
-                return -1;
+                perror("Unable to bind socket");
+                goto err_addr;
         }
 
         return 0;
+err_addr:
+        freeaddrinfo(skaddr);
+err_open:
+        return -1;
 }
 
 int
 ch_fini(void)
 {
+        close(sk);
+
         // TODO
         return -1;
 }
